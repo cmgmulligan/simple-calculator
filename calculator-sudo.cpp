8e31d6c0f4ad17ad17d7475d67838514da5d5498
david mulligan <dvdmlgn@outlook.com>
2018-07-27 22:36:19 +0100

    Add files via upload

---
+++ calculator-sudo.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>
+
+int numbers[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+char ops[] = {'+', '-', '*'}
+
+bool ValidateInput(char[]* validationChars) {
+    string userInput = GetUserInput();
+    int validNUmbers = 0;
+    char characters[] = userInput.ToArray();
+    double number;
+
+
+    for(int i = 0; i < characters.length; i++) {
+        
+        for(int j = 0; valiationChars.length; j++) {
+
+            if(characters[i] == valiationChars[j]) {
+                validNumbers++;
+                break;
+            }
+
+        }
+    }
+
+
+
+    if(characters.length == validNUmbers) {
+
+    } else {
+        std::cout << "you have not given me a valid number, please try again"
+
+    }
+
+}
+
+
+
+int doubleNumber(int number) {
+    return number * 2;
+}
+
+
+
+
+
+string GetUserInput() [
+    std::cout << "Enter in a number" << std::endl;
+    return std::cin;
+] 
+
+
+
+
+////////////////////////////////
+
+
+
+
+if (op != '+') break;
+
+
+
